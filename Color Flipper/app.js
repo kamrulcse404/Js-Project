a0533df612e3d141f8aee12646b912b74348ee8e
Md Kamrul Hasan <36786835+kamrulcse404@users.noreply.github.com>
2021-04-20 16:52:18 +0600

    first-commit

---
+++ Color Flipper/app.js
@@ -0,0 +1,24 @@
+const colors = [
+  "green",
+  "red",
+  "yellow",
+  "seagreen",
+  "tomato",
+  "rgba(133,122,200)",
+  "#f15025",
+  "rgba(119, 59, 143)",
+  "rgba(74, 68, 77)"
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
